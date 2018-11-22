5767882831423a9ac104293c3b4a0c1626c6ff0c
brunomb91 <bruno.mb.91@gmail.com>
2018-11-22 20:37:14 -0300

    Some changes

--- Lista_1/Q01/Q01.py
+++ Lista_1/Q01/Q01.py
@@ -8,13 +8,14 @@ img2[:,:,0] = 127
 img2[:,:,1] = 127
 img2[:,:,2] = 127
 
-"""
+
 cv2.imshow('img2', img2)
 cv2.imshow('img', img)
-"""
 
+"""
 cv2.imwrite("black_img.png", img)
 cv2.imwrite("gray_img.png", img2)
+"""
 
 k = cv2.waitKey(0)
 
